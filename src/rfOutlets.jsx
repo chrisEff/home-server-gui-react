bc3bb817bcc48fce0550d390b6b6436c90a34ab4
Christian Frenzl <chriseff@users.noreply.github.com>
2018-09-30 18:54:12 +0200

    added RF outlets

---
+++ src/rfOutlets.jsx
@@ -0,0 +1,59 @@
+'use strict';
+
+class RfOutlets extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      outlets: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`${config.api.protocol}://${config.api.host}:${config.api.port}/rfoutlets/outlet?key=${config.api.key}`)
+    const outlets = Object.values(await response.json())
+    this.setState({
+      outlets
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Steckdosen</h2>
+        {this.state.outlets.map(outlet => <RfOutlet key={outlet.name} outlet={outlet} />)}
+      </div>
+    )
+  }
+}
+
+class RfOutlet extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {outlet: props.outlet}
+  }
+  
+
+  render() {
+    const className = this.state.outlet.state ? 'outlet on' : 'outlet'
+    return (
+      <div className={className} onClick={this.handleClick}>
+        {this.state.outlet.name}
+        <div className='plug'>🔌</div>
+      </div>
+    )
+  }
+
+  handleClick = async () => {
+    const outlet = this.state.outlet
+    await fetch(
+      `${config.api.protocol}://${config.api.host}:${config.api.port}/rfoutlets/outlet/${outlet.id}/${outlet.state ? 0 : 1}?key=${config.api.key}`,
+      {method: 'PUT'}
+    )
+
+    outlet.state = outlet.state ? 0 : 1
+    this.setState({outlet})
+  }
+}
+
+
+ReactDOM.render(<RfOutlets/>, document.querySelector('#rfOutlets'));
