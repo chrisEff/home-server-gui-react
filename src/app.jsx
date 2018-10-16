8f155c9be495a53583141c014208d94df5b502b5
Christian Frenzl <chriseff@users.noreply.github.com>
2018-10-16 21:44:44 +0200

    loading css through style-loader + css-loader

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ const Tradfri = require('./tradfri/Tradfri')
 const RfOutlets = require('./rfOutlets/RfOutlets')
 const Temperature = require('./temperature/Temperature')
 
+require('style-loader!css-loader!./style.css')
+
 ReactDOM.render(<Tradfri/>, document.querySelector('#lights'));
 ReactDOM.render(<RfOutlets/>, document.querySelector('#rfOutlets'));
 ReactDOM.render(<Temperature/>, document.querySelector('#temperature'));
