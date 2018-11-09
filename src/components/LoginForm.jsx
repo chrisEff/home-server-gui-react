6e8d1a1c91aee9753ff5b09208a2ce054efbe58e
Christian Frenzl <chriseff@users.noreply.github.com>
2018-11-10 00:54:17 +0100

    added name attributes to login form fields (needed for 1Password)

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,10 +10,10 @@ class LoginForm extends React.Component {
 				<br/><br/>
 
 				API URL:<br/>
-				<input type='text' ref='apiUrl' style={{width: 250}}/><br/><br/><br/>
+				<input type='text' name='apiUrl' ref='apiUrl' style={{width: 250}}/><br/><br/><br/>
 
 				API Key:<br/>
-				<input type='text' ref='apiKey' style={{width: 250}}/><br/><br/><br/>
+				<input type='text' name='apiKey' ref='apiKey' style={{width: 250}}/><br/><br/><br/>
 
 				<button onClick={ () => this.props.onLogin(this.refs.apiUrl.value, this.refs.apiKey.value) }>OK</button>
 			</div>
