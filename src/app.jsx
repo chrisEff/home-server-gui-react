42c0e1744500d31b7886f6b22db0bdbabba25905
Christian Frenzl <chriseff@users.noreply.github.com>
2018-11-24 23:22:00 +0100

    code style

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Wrapper = require('./components/Wrapper')
 
+// eslint-disable-next-line import/no-webpack-loader-syntax
 require('style-loader!css-loader!./style.css')
 
 ReactDOM.render(<Wrapper />, document.querySelector('#wrapper'))
