3720015e8b8ab1c9787d86e54c1ddd20a267d165
Christian Frenzl <chriseff@users.noreply.github.com>
2018-12-16 20:26:26 +0100

    added redux dev tools helper

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import Wrapper from './components/Wrapper'
@@ -9,7 +9,10 @@ import Wrapper from './components/Wrapper'
 // eslint-disable-next-line import/no-webpack-loader-syntax
 require('style-loader!css-loader!./style.css')
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+	applyMiddleware(thunk)
+))
 
 render(
 	<Provider store={store}>
