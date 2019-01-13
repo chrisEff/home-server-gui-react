a10a66764e9e2f38f7ef747b428e0e17f04f4250
Christian Frenzl <chriseff@users.noreply.github.com>
2019-01-13 01:12:51 +0100

    login form looks a bit nicer now

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,17 +6,12 @@ class LoginForm extends React.Component {
 
 	render () {
 		return (
-			<div style={{textAlign: 'center'}}>
+			<div className='loginForm'>
 				<br/><br/>
 
-				API URL:<br/>
-				<input type='text' name='apiUrl' ref='apiUrl' style={{width: 250}}/><br/><br/><br/>
-
-				API User:<br/>
-				<input type='text' name='apiUser' ref='apiUser' style={{width: 250}}/><br/><br/><br/>
-
-				API Key:<br/>
-				<input type='password' name='apiKey' ref='apiKey' style={{width: 250}}/><br/><br/><br/>
+				<input placeholder='API URL' type='text' name='apiUrl' ref='apiUrl' /><br/>
+				<input placeholder='API User' type='text' name='apiUser' ref='apiUser' /><br/>
+				<input placeholder='API Key' type='password' name='apiKey' ref='apiKey' /><br/>
 
 				<button onClick={ () => this.props.onLogin(this.refs.apiUrl.value, this.refs.apiUser.value, this.refs.apiKey.value) }>OK</button>
 			</div>
