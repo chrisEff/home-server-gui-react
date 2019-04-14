5acd780c32f673d97f40e600a1383ffbebc39bd3
Christian Frenzl <chriseff@users.noreply.github.com>
2019-04-14 21:42:38 +0200

    fix for aphrodite breaking tests

--- test/jestSetup.js
+++ test/jestSetup.js
@@ -1,7 +1,8 @@
-require('@babel/polyfill')
+import '@babel/polyfill'
+import {StyleSheetTestUtils} from 'aphrodite'
 
 const Enzyme = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
 
 Enzyme.configure({adapter: new Adapter()})
-
+StyleSheetTestUtils.suppressStyleInjection()
