ffe8f91b025200da49d8e4aa08bfb83e57af8862
Christian Frenzl <chriseff@users.noreply.github.com>
2019-04-18 19:08:49 +0200

    separate webpack configs for dev + prod; not using source map on prod anymore, which makes the bundle MUCH smaller

---
+++ webpack.config.base.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+	entry: ['@babel/polyfill', './src/app.jsx'],
+	output: {
+		path: __dirname,
+		filename: './dist/bundle.js',
+	},
+	resolve: {
+		alias: {
+			// This is only for webpack. Separate alias for jest in jest.config.js.
+			'@': path.resolve(__dirname, 'src/'),
+		},
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				use: [
+					{loader: 'babel-loader'},
+				],
+			},
+		],
+	},
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,3 @@
-const path = require('path')
-
-module.exports = {
-	mode: 'production',
-	entry: ['@babel/polyfill', './src/app.jsx'],
-	output: {
-		path: __dirname,
-		filename: './dist/bundle.js',
-	},
-	resolve: {
-		alias: {
-			// This is only for webpack. Separate alias for jest in jest.config.js.
-			'@': path.resolve(__dirname, 'src/'),
-		},
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx$/,
-				use: [
-					{loader: 'babel-loader'},
-				],
-			},
-		],
-	},
-	devtool: 'cheap-module-eval-source-map',
+module.exports = (env) => {
+	return require(`./webpack.config.${env}.js`)
 }
