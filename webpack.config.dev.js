f6c1ae2989c1b384b38afa25fdbfbe8277f919af
Christian Frenzl <chriseff@users.noreply.github.com>
2019-04-19 16:07:34 +0200

    webpack dev config now sets NODE_ENV to "development"

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,14 @@
 const merge = require('webpack-merge')
+const webpack = require('webpack')
 const baseConfig = require('./webpack.config.base.js')
 
 module.exports = merge(baseConfig, {
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('development'),
+			},
+		}),
+	],
 	devtool: 'cheap-module-eval-source-map',
 })
